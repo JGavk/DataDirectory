ec796ab7f8953282ba718265b4fc58cbac97d439
JGavk <119267651+JGavk@users.noreply.github.com>
2023-11-20 14:12:18 -0500

    Update MainController.java

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -20,7 +20,7 @@ public class MainController {
     }
 
     public void addButton1(){
-        teacherForm = new TeacherForm(impDAO);
+        teacherForm = new TeacherForm(impDAO, visual);
 
     }
 }
