7acb5e61518ee9812571e2d996615594111fe597
JGavk <119267651+JGavk@users.noreply.github.com>
2023-11-20 16:47:36 -0500

    Update MainController.java

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import model.ImplementationDAO;
+import model.Teacher;
 import visual.TeacherForm;
 import visual.VisualRate;
 
@@ -10,17 +11,28 @@ public class MainController {
     private ImplementationDAO impDAO;
     private TeacherForm teacherForm;
 
-    public MainController(){
+    public MainController(VisualRate visual){
         this.visual = visual;
-        this.impDAO = impDAO;
+        this.impDAO = new ImplementationDAO();
         this.teacherForm = teacherForm;
     }
-    public void setVisual(){
-        this.visual=visual;
+
+    //Metodo para añadir los profesores desde TeacherForm al HashMap y a la tabla
+    public void addTeacherFromForm(Teacher teacher) {
+        impDAO.addTeacherFromUserInput(teacher);
+        visual.updateTeacherTable(teacher);
+        teacherForm.dispose();
     }
 
+    //Metodo para buscar los profesores desde el DAO en el controllador, evitar que se conozcan las frames y el DAO
+    public boolean teacherExists(int id) {
+        return impDAO.teacherExists(id);
+    }
+
+    //Boton que abre el panel de añadir
     public void addButton1(){
-        teacherForm = new TeacherForm(impDAO, visual);
+        teacherForm = new TeacherForm(this);
 
     }
+
 }
